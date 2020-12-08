4e187995b5002f42bdcd80f309446198cb1971c2
tompvr26 <tompoyvre@gmail.com>
2020-12-08 09:31:26 +0100

    classe car

--- Classe/Car.py
+++ Classe/Car.py
@@ -2,5 +2,5 @@ class Car:
     def __init__(self):
         self.list_passager = []
 
-    def add_promo(self, personne):
+    def add_passager(self, personne):
         self.list_passager.append(personne)
