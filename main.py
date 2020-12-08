70e751000936431204985a86fbe2dcc84358c559
Lucas Reteau <lucaporal@live.fr>
2020-12-08 13:30:39 +0100

    add window

--- main.py
+++ main.py
@@ -1,91 +1,14 @@
 import sys
 from PyQt5.QtWidgets import QApplication, QWidget
 
-from Classe.Ecole import Ecole
-from Classe.Promotion import Promotion
-from Classe.Eleve import Eleve
-from Classe.Professeur import Prof
-from Classe.Car import Car
+app = QApplication.instance()
+if not app:
+    app = QApplication(sys.argv)
 
-# Declaration d'un ecole
-ecole_epsi = Ecole("EPSI")
-
-
-# Déclaration des professeurs
-prof_python = Prof("MALABRY", "Emmanuel", "Python")
-prof_algo = Prof("BRAUX", "Mathias", "Algorithmie")
-prof_math = Prof("Robin", "Olivier", "Mathématique")
-
-# Déclaration des classes
-promotion_terminal_a = Promotion("Terminal A", 12, prof_algo)
-promotion_seconde_b = Promotion("Seconde B", 9, prof_math)
-promotion_premiere_c = Promotion("Première C", 14, prof_python)
-
-# Ajouter les promotions a l'ecole
-ecole_epsi.add_promo(promotion_seconde_b)
-ecole_epsi.add_promo(promotion_premiere_c)
-ecole_epsi.add_promo(promotion_terminal_a)
+fen1 = QWidget()
+fen1.setWindowTitle("Manager SchoolCar 2000")
+fen1.resize(800,500)
+fen1.show()
 
 
-
-# Déclaration des élèves
-eleve_tom = Eleve("Poyvre","Tom", ecole_epsi.nom_ecole(), False)
-eleve_lucas = Eleve("Reteau","Lucas", ecole_epsi.nom_ecole(), False)
-eleve_theo = Eleve("Zazou","Théo", ecole_epsi.nom_ecole(), False)
-eleve_florian = Eleve("Sinegre","Florian", ecole_epsi.nom_ecole(), False)
-eleve_coline = Eleve("Pierre","Coline", ecole_epsi.nom_ecole(), False)
-eleve_timote = Eleve("Dasque","Timoté", ecole_epsi.nom_ecole(), False)
-
-
-
-
-
-# Ajout des élèves dans les promoiton
-#Terminal A
-promotion_terminal_a.add_eleve(eleve_tom)
-promotion_terminal_a.add_eleve(eleve_lucas)
-promotion_terminal_a.add_eleve(eleve_theo)
-promotion_terminal_a.add_eleve(eleve_florian)
-promotion_terminal_a.add_eleve(eleve_coline)
-promotion_terminal_a.add_eleve(eleve_coline)
-promotion_terminal_a.add_eleve(eleve_coline)
-promotion_terminal_a.add_eleve(eleve_coline)
-
-#Premiere C
-
-promotion_premiere_c.add_eleve(eleve_tom)
-promotion_premiere_c.add_eleve(eleve_lucas)
-promotion_premiere_c.add_eleve(eleve_theo)
-promotion_premiere_c.add_eleve(eleve_florian)
-promotion_premiere_c.add_eleve(eleve_coline)
-promotion_premiere_c.add_eleve(eleve_coline)
-promotion_premiere_c.add_eleve(eleve_coline)
-promotion_premiere_c.add_eleve(eleve_coline)
-
-
-
-
-
-
-eleve_tom.eleve_present()
-eleve_theo.eleve_present()
-eleve_lucas.eleve_present()
-eleve_florian.eleve_present()
-eleve_coline.eleve_present()
-
-
-#Créer un car
-c1 = Car(50, ecole_epsi.nom_ecole(), "Marseille")
-
-#Ajout promo dans car
-c1.add_passager(promotion_terminal_a)
-c1.add_passager(promotion_premiere_c)
-
-
-
-
-
-
-
-# Faire l'appel
-#prof_algo.faire_appel(promotion_terminal_a)
+app.exec_()
--- main2.py
+++ main2.py
@@ -4,13 +4,11 @@ from Classe.Eleve import Eleve
 from Classe.Professeur import Prof
 from Classe.Car import Car
 
-
-
 # Declaration d'un ecole
 ecole_epsi = Ecole("EPSI")
 
 
-# Déclaration des professeurs #
+# Déclaration des professeurs
 prof_python = Prof("MALABRY", "Emmanuel", "Python")
 prof_algo = Prof("BRAUX", "Mathias", "Algorithmie")
 prof_math = Prof("Robin", "Olivier", "Mathématique")
@@ -19,6 +17,13 @@ prof_math = Prof("Robin", "Olivier", "Mathématique")
 promotion_terminal_a = Promotion("Terminal A", 12, prof_algo)
 promotion_seconde_b = Promotion("Seconde B", 9, prof_math)
 promotion_premiere_c = Promotion("Première C", 14, prof_python)
+
+# Ajouter les promotions a l'ecole
+ecole_epsi.add_promo(promotion_seconde_b)
+ecole_epsi.add_promo(promotion_premiere_c)
+ecole_epsi.add_promo(promotion_terminal_a)
+
+
 
 # Déclaration des élèves
 eleve_tom = Eleve("Poyvre","Tom", ecole_epsi.nom_ecole(), False)
@@ -30,10 +35,7 @@ eleve_timote = Eleve("Dasque","Timoté", ecole_epsi.nom_ecole(), False)
 
 
 
-# Ajouter les promotions a l'ecole
-ecole_epsi.add_promo(promotion_seconde_b)
-ecole_epsi.add_promo(promotion_premiere_c)
-ecole_epsi.add_promo(promotion_terminal_a)
+
 
 # Ajout des élèves dans les promoiton
 #Terminal A
@@ -42,28 +44,40 @@ promotion_terminal_a.add_eleve(eleve_lucas)
 promotion_terminal_a.add_eleve(eleve_theo)
 promotion_terminal_a.add_eleve(eleve_florian)
 promotion_terminal_a.add_eleve(eleve_coline)
+promotion_terminal_a.add_eleve(eleve_coline)
+promotion_terminal_a.add_eleve(eleve_coline)
+promotion_terminal_a.add_eleve(eleve_coline)
+
+#Premiere C
+
+promotion_premiere_c.add_eleve(eleve_tom)
+promotion_premiere_c.add_eleve(eleve_lucas)
+promotion_premiere_c.add_eleve(eleve_theo)
+promotion_premiere_c.add_eleve(eleve_florian)
+promotion_premiere_c.add_eleve(eleve_coline)
+promotion_premiere_c.add_eleve(eleve_coline)
+promotion_premiere_c.add_eleve(eleve_coline)
+promotion_premiere_c.add_eleve(eleve_coline)
+
+
+
+
+
 
 eleve_tom.eleve_present()
 eleve_theo.eleve_present()
 eleve_lucas.eleve_present()
 eleve_florian.eleve_present()
+eleve_coline.eleve_present()
 
 
 #Créer un car
-c1 = Car(3)
-
+c1 = Car(50, ecole_epsi.nom_ecole(), "Marseille")
 
 #Ajout promo dans car
 c1.add_passager(promotion_terminal_a)
-
-
-
-
-
-
+c1.add_passager(promotion_premiere_c)
 
 
 # Faire l'appel
 #prof_algo.faire_appel(promotion_terminal_a)
-
-
