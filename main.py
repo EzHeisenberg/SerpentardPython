042338168ffbce1829bf0b4f58413f52c9e10a74
Lucas Reteau <lucaporal@live.fr>
2020-12-09 17:56:19 +0100

    add coucou

--- main.py
+++ main.py
@@ -134,22 +134,3 @@ eleve_timote = Eleve("Dasque","Timoté", ecole_epsi.nom_ecole(), False)
 
 
 
-
-
-#Créer un car
-=======
-import sys
-from PyQt5.QtWidgets import QApplication, QWidget
-
-app = QApplication.instance()
-if not app:
-    app = QApplication(sys.argv)
-
-fen1 = QWidget()
-fen1.setWindowTitle("Manager SchoolCar 2000")
-fen1.resize(800,500)
-fen1.show()
-
-
-app.exec_()
-
